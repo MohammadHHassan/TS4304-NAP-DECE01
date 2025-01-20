54d48f8cafede4434aa0ee0dc6d6cb920f88c04f
Mohammad Hj Hassan <35444902+MohammadHHassan@users.noreply.github.com>
2025-01-20 14:58:49 +0800

    Add files via upload

---
+++ day4/LearningArrays2.java
@@ -0,0 +1,33 @@
+/*	Create an array which consists of birth month names (January, February.....)
+ * 	Ask the user to enter their birth month value
+ * 	Print out their birth month name using the array and its value
+ */
+
+package day4;
+
+import java.util.Scanner;
+
+public class LearningArrays2
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		
+		String [] month = {"January", "February", "March", "April", "May", "June", "July",
+							"August", "September", "October", "November", "December"};
+		
+		System.out.print("Please enter your birth month value: ");
+		int value = scanner.nextInt();
+		
+		if(value>=1 && value<=12)
+		{
+			System.out.println("Your birth month is " + month[value-1]);
+		}
+		else
+		{
+			System.out.println("Invalid value.");
+		}
+	}
+}
+
+
